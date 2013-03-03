857a08dd55e081ff4c33db12ff4b67451494d736
Alon Daks <Alon.Daks@gmail.com>
2013-03-03 00:25:33 -0800

    created match model

--- poolrank/ranking_app/models.py
+++ poolrank/ranking_app/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Ranking(models.Model):
 	rating = models.DecimalField(max_digits=21, decimal_places=15)
+	num_wins = models.IntegerField()
+	num_matches = models.IntegerField()
 
-
-# class Match(models.Model):
-
-# class User(models.Model):
-
+class Match(models.Model):
+	winner_id = models.IntegerField()
+	looser_id = models.IntegerField()
+	date = models.DateField(auto_now=False, auto_now_add=True)
