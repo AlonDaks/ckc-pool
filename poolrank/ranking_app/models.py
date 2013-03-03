9b8585f4a60377d9b76f5e882d7817a75e883350
Alon Daks <Alon.Daks@gmail.com>
2013-03-03 15:48:06 -0800

    added user foreign key to Ranking Model

--- poolrank/ranking_app/models.py
+++ poolrank/ranking_app/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Ranking(models.Model):
 	rating = models.DecimalField(max_digits=21, decimal_places=15)
 	num_wins = models.IntegerField()
 	num_matches = models.IntegerField()
+	user = models.Foreignkey('User')
 
 class Match(models.Model):
 	winner_id = models.IntegerField()
